a9efdb89357e5d313fcb1f0a34d743fd5eb68092
Duncan Sparks <duncan.sprks@gmail.com>
2020-02-01 09:54:47 -0800

    Buncha stuff

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,13 +5,20 @@ const Speed: float = 100.0
 var velocity := Vector2.ZERO
 
 enum Direction {Up, Down, Left, Right}
-var face: int = Direction.Up
+var face: int = Direction.Down
+var walking := false
+
+var health: int = 5
 
 var bullet_available := true
 
 const bullet_ref := preload("res://Prefabs/PlayerBullet.tscn")
 
+#export(AudioStream) var kick_sound
+
 onready var spr := $Sprite as AnimatedSprite
+onready var sound_kick := $SoundKick as AudioStreamPlayer
+onready var healthbar := $Healthbar as TextureProgress
 
 # ======================================================
 
@@ -26,12 +33,16 @@ func _process(delta):
 	velocity.x = xx
 	velocity.y = yy
 	
+	walking = velocity != Vector2.ZERO
+	
 	direction_management()
 	sprite_management()
 	
 	if Input.is_action_just_pressed("attack") and bullet_available:
 		throw_bullet()
 		bullet_available = false
+	
+	healthbar.set_value(health)
 
 
 func _physics_process(delta):
@@ -41,6 +52,10 @@ func _physics_process(delta):
 
 func set_bullet_available(value: bool):
 	bullet_available = value
+	
+	
+func damage(amount: int):
+	health -= amount
 
 # ======================================================
 
@@ -62,15 +77,21 @@ func direction_management():
 				
 				
 func sprite_management():
+	var anim: String
 	match face:
 		Direction.Up:
-			spr.play("up")
+			anim = "up"
 		Direction.Down:
-			spr.play("down")
+			anim = "down"
 		Direction.Left:
-			spr.play("left")
+			anim = "left"
 		Direction.Right:
-			spr.play("right")
+			anim = "right"
+			
+	if walking:
+		anim += "_walk"
+		
+	spr.play(anim)
 			
 			
 func throw_bullet():
@@ -78,3 +99,4 @@ func throw_bullet():
 	bullet.set_position(get_position())
 	bullet.set_global_rotation(get_position().direction_to(get_global_mouse_position()).angle())
 	get_tree().get_root().add_child(bullet)
+	sound_kick.play()
--- Scripts/PlayerBullet.gd
+++ Scripts/PlayerBullet.gd
@@ -8,26 +8,29 @@ var can_pick_up := false
 
 func _ready():
 	linear_velocity = Vector2(speed, 0).rotated(global_rotation)
+	set_bounce(0.8)
+
 
 func damage(body):
 	if body.is_in_group("enemy"):
 		body.health -= damage
 
-func collide(body):
-	damage(body)
-	get_node('AnimatedSprite').play('die')
-
-
-func _animation_finished(animation):
-	if animation == 'die':
-		queue_free()
-
 
 func _on_AreaPickUp_body_entered(body: Node):
 	if body.is_in_group("Player") and can_pick_up:
+		$SoundPickUp.play()
 		body.set_bullet_available(true)
-		queue_free()
+		$Sprite.hide()
+		$PartsIdle.set_emitting(false)
+		$PartsPickUp.set_emitting(true)
+		$TimerDestroy.start()
+		can_pick_up = false
 
 
 func _on_TimerPickUp_timeout():
 	can_pick_up = true
+
+
+func _on_PlayerBullet_body_entered(body):
+	if body.is_in_group("Enemy"):
+		body.hit()
