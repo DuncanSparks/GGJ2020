4ed0f7f0f9f4875cd7f1f67ca6a47142ba614292
Duncan Sparks <duncan.sprks@gmail.com>
2020-01-31 19:37:41 -0800

    Continue on player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,8 +9,8 @@ func _ready():
 
 
 func _process(delta):
-	var xx := int(Input.is_action_just_pressed("move_right")) - int(Input.is_action_just_pressed("move_left"))
-	var yy := int(Input.is_action_just_pressed("move_down")) - int(Input.is_action_just_pressed("move_up"))
+	var xx := int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	var yy := int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	
 	velocity.x = xx
 	velocity.y = yy
