012ff8b4606f07460881eb2cf4d9d652e33adb7a
Duncan Sparks <duncan.sprks@gmail.com>
2020-01-31 19:27:13 -0800

    Continue on player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,9 +2,19 @@ extends KinematicBody2D
 
 const Speed: float = 100.0
 
+var velocity := Vector2.ZERO
+
 func _ready():
 	pass
 
 
 func _process(delta):
-	pass
+	var xx := int(Input.is_action_just_pressed("move_right")) - int(Input.is_action_just_pressed("move_left"))
+	var yy := int(Input.is_action_just_pressed("move_down")) - int(Input.is_action_just_pressed("move_up"))
+	
+	velocity.x = xx
+	velocity.y = yy
+
+
+func _physics_process(delta):
+	move_and_slide(velocity * Speed)
