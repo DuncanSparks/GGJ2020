b4711603e0eab71a4a59eb7394aed28c2aab35b9
Ken Zhou <kxzhou@uci.edu>
2020-01-31 20:09:29 -0800

    Bullet stuff
    Bullet has less friction, has an animation

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -10,3 +10,12 @@ func _ready():
 func damage(body):
 	if body.is_in_group("enemy"):
 		body.health -= damage
+
+func collide(body):
+	damage(body)
+	get_node('AnimatedSprite').play('die')
+
+
+func _animation_finished(animation):
+	if animation == 'die':
+		queue_free()
