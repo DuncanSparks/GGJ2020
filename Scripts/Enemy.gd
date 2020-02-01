1c5c90e3467fcca821152383206f90de798b6ad6
janderson152 <44076627+janderson152@users.noreply.github.com>
2020-01-31 20:17:29 -0800

    Create Enemy.gd
    Started Enemy Script

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,57 @@
+extends KinematicBody2D
+
+const Speed: float = 100.0
+
+var velocity := Vector2.ZERO
+
+enum Direction {Up, Down, Left, Right}
+var face: int = Direction.Up
+
+onready var spr := $Sprite as AnimatedSprite
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	var xx := int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	var yy := int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	
+	velocity.x = xx
+	velocity.y = yy
+	
+	direction_management()
+	sprite_management()
+
+
+func _physics_process(delta):
+	move_and_slide(velocity * Speed)
+
+
+func direction_management():
+	var prev_face := face
+	
+	if velocity.x == 0:
+		match velocity.y:
+			-1.0:
+				face = Direction.Up
+			1.0:
+				face = Direction.Down
+	elif velocity.y == 0:
+		match velocity.x:
+			-1.0:
+				face = Direction.Left
+			1.0:
+				face = Direction.Right
+				
+				
+func sprite_management():
+	match face:
+		Direction.Up:
+			spr.play("up")
+		Direction.Down:
+			spr.play("down")
+		Direction.Left:
+			spr.play("left")
+		Direction.Right:
+			spr.play("right")
