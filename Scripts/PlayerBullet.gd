37ee301b46eae18e00c6ce3486db840df301c3f4
Duncan Sparks <duncan.sprks@gmail.com>
2020-01-31 21:03:21 -0800

    Add player bullet throwing

--- Scripts/PlayerBullet.gd
+++ Scripts/PlayerBullet.gd
@@ -4,6 +4,8 @@ extends RigidBody2D
 export var speed: int = 5
 export var damage: int = 5
 
+var can_pick_up := false
+
 func _ready():
 	linear_velocity = Vector2(speed, 0).rotated(global_rotation)
 
@@ -19,3 +21,13 @@ func collide(body):
 func _animation_finished(animation):
 	if animation == 'die':
 		queue_free()
+
+
+func _on_AreaPickUp_body_entered(body: Node):
+	if body.is_in_group("Player") and can_pick_up:
+		body.set_bullet_available(true)
+		queue_free()
+
+
+func _on_TimerPickUp_timeout():
+	can_pick_up = true
