1d29af2bb3acbfdcbb66d5b9db16523a94f66e03
Ken Zhou <kxzhou@uci.edu>
2020-01-31 19:47:09 -0800

    Bullets deal damage
    They actually hurt now

--- Bullet.gd
+++ Bullet.gd
@@ -1,11 +1,12 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#Speed
 export var speed: int = 5
+export var damage: int = 5
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	linear_velocity = Vector2(speed, 0).rotated(global_rotation)
+
+func damage(body):
+	if body.is_in_group("enemy"):
+		body.health -= damage
